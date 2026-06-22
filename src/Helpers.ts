856c3a3e00a41caa6ac38f4998bedb92e22ea7a9
Md Saifuddin <saifuddinbabu@gmail.com>
2026-06-22 19:33:40 +0600

    reorganized and fixed some error

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -1,8 +1,12 @@
 import moment from 'moment-timezone';
 import fs from "fs"
 import path from 'path';
+import axios from 'axios';
+import { JSDOM } from 'jsdom';
+import https from "https";
+import 'dotenv/config'
 
-export const normalOpenTime = ():boolean =>{
+export const normalOpenTime = (): boolean => {
   // Get current time in Dhaka timezone
   const now = moment().tz("Asia/Dhaka");
 
@@ -10,13 +14,13 @@ export const normalOpenTime = ():boolean =>{
   const day = now.day();   // 0 = Sunday, 1 = Monday, ... 6 = Saturday
 
   // Allow only between 10AM (10) and 2PM (14), excluding Friday (5) and Saturday (6)
-  const withinTime = hour >= 10 && hour < 14;
+  const withinTime = hour >= 10 && hour < 15;
   const notWeekend = day !== 5 && day !== 6;
 
   return withinTime && notWeekend;
 }
 
-export const saveArrayToFile = async (dataArray:any,targetDir:string):Promise<void> =>{
+export const saveArrayToFile = async (dataArray: any, targetDir: string): Promise<void> => {
   // Ensure directory exists (create if missing)
   fs.mkdirSync(targetDir, { recursive: true });
 
@@ -38,16 +42,16 @@ export const saveArrayToFile = async (dataArray:any,targetDir:string):Promise<vo
 
   console.log(`Data saved to ${filename}`);
 
-  
+
 }
 
-export const parseCustomDate = (dateStr:string):Date|null=> {
+export const parseCustomDate = (dateStr: string): Date | null => {
   // Remove the "at" for easier parsing
   const cleaned = dateStr.replace(" at ", " ");
-  
+
   // Create a Date object
   const date = new Date(cleaned);
-  
+
   // Check if parsing succeeded
   if (isNaN(date.getTime())) {
     return null;
@@ -57,7 +61,7 @@ export const parseCustomDate = (dateStr:string):Date|null=> {
   return date; // You can also return date.getTime() for timestamp
 }
 
-export const getLatestSavedData = async(targetDir:string):Promise<any> => {
+export const getLatestSavedData = async (targetDir: string): Promise<any> => {
   const filename = path.join(targetDir, `data_latest.json`);
   // Read file synchronously
   const rawData = fs.readFileSync(filename, "utf-8");
@@ -67,7 +71,7 @@ export const getLatestSavedData = async(targetDir:string):Promise<any> => {
   return jsonData ?? [];
 }
 
-export const formatDate =( date: Date = new Date(), format: string = "YYYY-MM-DD HH:mm:ss"): string => {
+export const formatDate = (date: Date = new Date(), format: string = "YYYY-MM-DD HH:mm:ss"): string => {
   const pad = (n: number): string => String(n).padStart(2, "0");
 
   const year: number = date.getFullYear();
@@ -84,4 +88,34 @@ export const formatDate =( date: Date = new Date(), format: string = "YYYY-MM-DD
     .replace("HH", hours)
     .replace("mm", minutes)
     .replace("ss", seconds);
+}
+
+export const getAllStockPrice = async (): Promise<any> => {
+  const stockUrl = process.env.ALL_STOCK_PRICE_URL ?? "";
+  const res = await axios.get(stockUrl, {
+    httpsAgent: new https.Agent({ rejectUnauthorized: false })
+  });
+  const dom = new JSDOM(res.data);
+  const tableJson: any = [];
+  const tableHeaders = ["sl", "TRADING_CODE", "LTP", "HIGH", "LOW", "CLOSEP", "YCP", "CHANGE", "TRADE", "VALUE", "VOLUME"];
+  const tableContent = dom.window.document.querySelectorAll(".table-responsive.inner-scroll tbody")
+  if (tableContent.length > 0) {
+
+    tableContent.forEach((el, index) => {
+      const tableRow: any = {}
+      if (el.querySelectorAll("td").length > 0)
+        el.querySelectorAll("td").forEach((el2, in2) => {
+          tableRow[tableHeaders[in2]] = el2?.textContent.replace(/[\t\n]/g, '');
+        })
+      tableJson[index] = tableRow;
+    })
+    // console.log({totaldata:tableJson})
+    // saveArrayToFile(tableJson,'./data');
+
+
+  }
+  const lastUpdateText = dom.window.document.querySelector("h2.BodyHead.topBodyHead")?.textContent?.replace('Latest Share Price On ', '');
+  const lastUpdateTime = parseCustomDate(lastUpdateText ?? '');
+  console.log({ lastUpdateTime: lastUpdateTime });
+  return { lastUpdateTime: lastUpdateTime, marketStatus: true, table: tableJson }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,36 @@
 /* import { dirname } from 'path';
-import { fileURLToPath } from 'url'; */
+import { fileURLToPath } from 'url'; 
 import axios from "axios";
-import https from "https";
+import https from "https";*/
 import http from "http";
-import cors from "cors";  
-import {Server } from "socket.io";
-import {JSDOM} from "jsdom"
+import cors from "cors";
+import { Server } from "socket.io";
+// import {JSDOM} from "jsdom"
 import express from "express";
-import {normalOpenTime, saveArrayToFile, parseCustomDate, getLatestSavedData} from "./Helpers"
+import path from 'path';
+import 'dotenv/config'
+import { normalOpenTime, saveArrayToFile, getLatestSavedData, getAllStockPrice } from "./Helpers.js"
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors());
 
+// Serve static files from the "data" directory
+app.use("/data", express.static(path.join(__dirname, "../data")));
+
+// Example route
+app.get("/", (_req, res) => {
+  res.send("JSON server is running. Try /data/data_latest.json");
+});
+
 const server = http.createServer(app);
+console.log({ FRONT_END_HOST: process.env.FRONT_END_HOST });
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRON_END_HOST, // your React app's URL (Vite default)
+    origin: process.env.FRONT_END_HOST, // your React app's URL (Vite default)
     methods: ['GET', 'POST'],
   },
 });
@@ -28,25 +42,39 @@ io.on('connection', (socket) => {
     console.log('Client disconnected:', socket.id);
   });
 });
+let cachedData: any = false;
 
 const main = async (): Promise<void> => {
-  if(!normalOpenTime()){
-    console.log("dse is off");
-    const fileData = await getLatestSavedData('./data');
-    if(fileData){
-      const payload = {
-            message: "Latest share price",
-            time: new Date().toISOString(),
-            data:fileData
-          };
+  if (!normalOpenTime()) {
 
-          console.log('Broadcasting:',new Date().toISOString());
-          io.emit('server_update', payload);
+    let fileData = cachedData?.table ?? await getLatestSavedData('./data');
+    if (!fileData?.data) {
+      const data = await getAllStockPrice();
+
+      await saveArrayToFile(data, './data');
+      if (data?.lastUpdateTime != null) {
+        fileData = data;
+      }
     }
-    
-     return;
+    console.log("dse is off", cachedData?.table ? "cache data available" : "file data");
+    if (fileData) {
+      if (!cachedData?.table)
+        cachedData = fileData;
+      // console.log({cachedData});
+
+      const payload = {
+        message: "Latest share price",
+        time: new Date().toISOString(),
+        data: { ...cachedData, marketStatus: false }
+      };
+
+      console.log('Broadcasting:', new Date().toISOString());
+      io.emit('server_update', payload);
+    }
+
+    return;
   }
-  const res = await axios.get("https://www.dsebd.org/latest_share_price_scroll_l.php", {
+  /* const res = await axios.get("https://www.dsebd.org/latest_share_price_scroll_l.php", {
     httpsAgent: new https.Agent({ rejectUnauthorized: false })
   });
   const dom = new JSDOM(res.data);
@@ -71,19 +99,21 @@ const main = async (): Promise<void> => {
       const lastUpdateText = dom.window.document.querySelector("h2.BodyHead.topBodyHead")?.textContent?.replace('Latest Share Price On ','');
       const lastUpdateTime = parseCustomDate(lastUpdateText??'');
       console.log({lastUpdateTime:lastUpdateTime });
-      const data = {lastUpdateTime:lastUpdateTime,table: tableJson}
-      
-      await saveArrayToFile(data,'./data');
-      if(lastUpdateTime!= null){
-          const payload = {
-            message: "Latest share price",
-            time: new Date().toISOString(),
-            data
-          };
+      const data = {lastUpdateTime:lastUpdateTime, marketStatus:true, table: tableJson} */
 
-          console.log('Broadcasting:',new Date().toISOString());
-          io.emit('server_update', payload);
-      }
+  const data = await getAllStockPrice();
+
+  await saveArrayToFile(data, './data');
+  if (data?.lastUpdateTime != null) {
+    const payload = {
+      message: "Latest share price",
+      time: new Date().toISOString(),
+      data
+    };
+
+    console.log('Broadcasting:', new Date().toISOString());
+    io.emit('server_update', payload);
+  }
   // console.log(dom.window.document.querySelector(".table-responsive.inner-scroll tbody")?.textContent);
 };
 
